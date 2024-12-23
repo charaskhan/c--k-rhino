75e2474574495b81989586efa602cfc9d3ab9e66
Charas khan <134779686+charaskhan@users.noreply.github.com>
2024-12-23 15:49:08 +0500

    Add files via upload

---
+++ commons/business-header.php
@@ -0,0 +1,50 @@
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i"
+          rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../view/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="../view/css/animate.css" type="text/css">
+    <link rel="stylesheet" href="../view/css/responsive.css" type="text/css">
+    <link rel="stylesheet" href="../view/css/index.css" type="text/css">
+
+</head>
+<body>
+
+<div class="clearfix">
+
+    <header id="header" class="black">
+
+        <div class="container clearfix">
+
+            <div id="logo">
+                <a href="business-home.php"><img src="images/logo1.png" alt="Canvas Logo"></a>
+            </div>
+
+            <nav id="primary-menu">
+
+                <ul style="touch-action: pan-y;">
+                    <li class=""><a href="#">
+                            <div>My Products</div>
+                        </a></li>
+                    <li><a href="create-discount.php">
+                            <div>Create Discount</div>
+                        </a></li>
+                    <li><a href="business-customers.php" >
+                            <div>Customers</div>
+                        </a></li>
+                    <li>
+                        <a href="#" >
+                            <div>Profile</div>
+                        </a></li>
+                    <li><a href="../controller/signout.php">
+                            <div>Log Out</div>
+                        </a></li>
+                </ul>
+
+            </nav>
+
+        </div>
+
+
+    </header>
+
+</div>
